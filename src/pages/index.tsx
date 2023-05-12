37ed3326901b15adc96061532cd9d072ad37633c
Martin Minkov <minkovlmartin@gmail.com>
2023-05-12 13:23:53 -0700

    refactor(index.tsx): change question format from question and answer to text format in explain option to improve clarity and consistency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,12 +23,13 @@ const Home: NextPage = () => {
   const makeQuery = async () => {
     let query;
     if (selectedOption === "explain") {
-      query = `Explain the following question in a question and answer format. ${question}`;
+      query = `Explain the following question in a text format. ${question}`;
     } else if (selectedOption === "code") {
       query = `Write a TypeScript code example answering the question. ${question}`;
     } else {
       return;
     }
+    console.log(query);
 
     try {
       const response = await mutation.mutateAsync({ query });
